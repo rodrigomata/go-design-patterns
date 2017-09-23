30486db8a85fad4b30f1fed2e36aa66cb23f5c55
Rodrigo Mata <rodrigo.mata.ib@gmail.com>
2017-09-23 02:40:23 -0500

    feat: added singleton pattern

--- creational/singleton/singleton.go
+++ creational/singleton/singleton.go
@@ -1,1 +1,14 @@
 package singleton
+
+type singleton struct {
+}
+
+var instance *singleton
+
+// GetInstance checks if an instance exists or creates one
+func GetInstance() *singleton {
+	if instance == nil {
+		instance = new(singleton) // or &singleton{} <-- Both are not thread safe
+	}
+	return instance
+}
